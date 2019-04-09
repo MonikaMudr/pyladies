9e44f5e97d7525497d92920208074327a12493c0
Monika Dvorakova <monika.mudrochova@gmail.com>
2019-04-09 14:53:42 +0200

    Verze s funkci.Funkce ma jako argument text pisne a pocitane pismeno

--- 05/ukol_6.py
+++ 05/ukol_6.py
@@ -1,4 +1,4 @@
-text_pisne = '''Just a young gun with a quick fuse
+song_lyrics = '''Just a young gun with a quick fuse
 I was uptight, wanna let loose
 I was dreaming of bigger things
 And wanna leave my own life behind
@@ -25,8 +25,12 @@ They say you're basic, they say you're easy
 You're always riding in the back seat
 Now I'm smiling from the stage while
 You were clapping in the nose bleeds'''
-count = 0
-for i in text_pisne:
-    if i == 'k' or i == "K":
-        count += 1
-print(count)
+
+def count_letter(song_lyrics, letter):
+    count = 0
+    for i in song_lyrics:
+        if i.lower() == letter:
+         count += 1
+    print(count)
+
+count_letter(song_lyrics, "k")
