7b03e95959b048ce8680feeef54985e14a46921a
Monika Dvorakova <monika.mudrochova@gmail.com>
2019-04-16 21:56:34 +0200

    Smazani souboru piskvorky(soubor bez strategie), smazany zbytecne importy v modulu hra.py

--- piskvorky/hra.py
+++ piskvorky/hra.py
@@ -1,6 +1,4 @@
-from piskvorky_strategie import tah_hrace, tah, vyhodnot, piskvorky1D
-from random import randrange
-from ai import tah_pocitace_bez_strategie, tah_pocitace_strategie_vlevo, tah_pocitace_strategie_vpravo, tah_pocitace
+from piskvorky_strategie import piskvorky1D
 
 
 piskvorky1D()
