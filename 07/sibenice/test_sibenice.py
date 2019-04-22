5e20746bb49b299a929c59facfde1d7f3590b6ec
Monika Dvorakova <monika.mudrochova@gmail.com>
2019-04-22 22:03:10 +0200

    Upraveny testy k upravenym funkcim v sibenici

--- 07/sibenice/test_sibenice.py
+++ 07/sibenice/test_sibenice.py
@@ -1,5 +1,5 @@
 
-from sibenice_funkce import vyber_slova, je_pismeno_ve_slove, vloz_pismeno, vyhodnot_prubeh_hry
+from sibenice_funkce import vyber_slova, kolikrat_ve_slove, vloz_pismeno, vyhodnot_prubeh_hry
 
 def test_vyhodnot_prubeh_hry():
     assert vyhodnot_prubeh_hry('---', 4) == 'pokracuj'
@@ -9,9 +9,9 @@ def test_vyhodnot_prubeh_hry():
 def test_vyber_slova():
     assert vyber_slova() == 'pes' or 'balon' or 'mispule'
 
-def test_je_pismeno_ve_slove():
-    assert je_pismeno_ve_slove('s', 'pes') == True
-    assert je_pismeno_ve_slove('l', 'pes') == False
-
 def test_vloz_pismeno():
-    assert vloz_pismeno('---', 's', 'pes') == '--s'
+    assert vloz_pismeno('---', 's', 'pes', 1) == '--s'
+    assert vloz_pismeno('---', 'o', 'oko', 2) == 'o-o'
+
+def test_kolikrat_ve_slove():
+    assert kolikrat_ve_slove('o', 'oko') == 2
