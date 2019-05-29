a36973608b7fcbd524ea85b657e8e7a3b9a73cf0
Monika Dvorakova <monika.mudrochova@gmail.com>
2019-05-29 14:11:03 +0200

    Prvni zakladni funkcni verze miniskolky s pygletem.

---
+++ miniskolka_pyglet/miniskolka2.py
@@ -0,0 +1,250 @@
+"""This is a neverending game. Your task is look after a group of children in
+a kindergarden. State of every child is defined by its mood, hunger and
+energy. Every round you can change a state of a child by doing different
+activity. Every round you select a child and an activity.
+"""
+
+import pyglet
+from pyglet import window, clock
+from miniskolka_functions2 import (
+                                   create_dic_activities, reset, wait,
+                                   create_dic_children, create_batch_labels,
+                                   children_set_position, create_icons_children, activity_election)
+from miniskolka_classes_activity import (Child, Boy, Girl, Activity,
+                                         SleepActivity, PlayActivity,
+                                         EatActivity, PlayWithFriend)
+from pyglet.window import key
+
+pyglet.resource.path = ['./resources']
+pyglet.resource.reindex()
+height = 460
+width = 1280
+batch_objects = pyglet.graphics.Batch()
+meals = ['pancake', 'icecream', 'chips', 'pasta']
+toys = ['dolly', 'barbie', 'lego', 'car']
+
+
+# loading images
+classroom = pyglet.resource.image('trida2a.jpg')
+icon = pyglet.resource.image('icon.png')
+bed_image = pyglet.resource.image('bed200.png')
+table_image = pyglet.resource.image('table90.png')
+chair_image = pyglet.resource.image('chair110.png')
+bear_image = pyglet.resource.image('bear50.png')
+train_image = pyglet.resource.image('train50.png')
+bebe_image = pyglet.resource.image('bebe50.png')
+ball_image = pyglet.resource.image('ball50.png')
+cubes_image = pyglet.resource.image('cubes70.png')
+
+# loading images of children
+maruska_happy = pyglet.resource.image('maruska150.png')
+maruska_sad = pyglet.resource.image('maruska_sad_110.png')
+kacenka_happy = pyglet.resource.image('kacenka150.png')
+kacenka_sad = pyglet.resource.image('kacenka_sad_110.png')
+pepicek_happy = pyglet.resource.image('pepicek150.png')
+pepicek_sad = pyglet.resource.image('pepicek_sad_110.png')
+jenicek_happy = pyglet.resource.image('jenicek_150.png')
+jenicek_sad = pyglet.resource.image('jenicek_sad_110.png')
+
+# creating sprites of the things
+bed2 = pyglet.sprite.Sprite(bed_image, x=width//6, y=height//6,
+                            batch=batch_objects)
+table = pyglet.sprite.Sprite(table_image, x=width//80, y=height//6,
+                             batch=batch_objects)
+chair = pyglet.sprite.Sprite(chair_image, x=width//60, y=height//6,
+                             batch=batch_objects)
+bear = pyglet.sprite.Sprite(bear_image, x=width - 50, y=height//6,
+                            batch=batch_objects)
+train = pyglet.sprite.Sprite(train_image, x=width - 80, y=height//6,
+                             batch=batch_objects)
+bebe = pyglet.sprite.Sprite(bebe_image, x=width - 120, y=height//6,
+                            batch=batch_objects)
+ball = pyglet.sprite.Sprite(ball_image, x=width - 200, y=height//6,
+                            batch=batch_objects)
+cubes = pyglet.sprite.Sprite(cubes_image, x=width - 250, y=height//6,
+                             batch=batch_objects)
+
+
+children = []
+children.append(Girl('Maruska', 'pancake', 'dolly',
+                     maruska_sad, maruska_happy))
+children.append(Girl('Kacenka', 'icecream', 'barbie',
+                     kacenka_sad, kacenka_happy))
+children.append(Boy('Jenicek', 'pasta', 'lego',
+                    jenicek_sad, jenicek_happy))
+children.append(Boy('Pepicek', 'chips', 'car',
+                    pepicek_sad, pepicek_happy))
+
+dic_children = create_dic_children(children)
+
+activities = []
+activities.append(SleepActivity('sleep'))
+activities.append(PlayActivity('play', 'toy'))
+activities.append(EatActivity('eat', 'food'))
+activities.append(PlayWithFriend('play with a friend', 'other_child'))
+
+dic_activities = create_dic_activities(activities)
+
+chosen_child_activity = [children[1], None]
+game_label = pyglet.text.Label('', x=15, y=38, font_size=14, bold=True, multiline=True, width=1200)
+
+def reset2(t):
+    game_label.text = initial_text
+    children_set_position(children)
+    chosen_child_activity[0] = children[1]
+
+# creating labels
+def create_initial_text():
+    initial_text = """Press right/left key to choose the child.\nWhat do you want {} to do. Press the corresponding number {} """.format(chosen_child_activity[0].name, activity_election(dic_activities))
+    return initial_text
+
+initial_text = create_initial_text()
+
+def reset3(game_label, initial_text, children):
+    game_label.text = initial_text
+    children_set_position(children)
+
+reset(chosen_child_activity)
+reset3(game_label, initial_text, children)
+
+
+
+
+window = pyglet.window.Window(1280, 500, 'Miniskolka',
+                              style=window.Window.WINDOW_STYLE_DEFAULT)
+
+
+    
+@window.event
+def on_draw():
+    window.clear()
+    classroom.blit(0, 58)
+    '''window.set_icon(icon)'''
+    batch_objects.draw()
+    create_batch_labels(children)
+    for child in children:
+        child.sprite.draw()
+    icons = create_icons_children(children)
+    for icon2 in icons:
+        icon2.draw()
+    game_label.draw()
+
+
+## napsat jako metodu aktivity, ktera vybere ze seznamu children 
+@window.event
+def on_key_press(symbol, modifiers):
+        """Defines how the child and activity are chosen."""
+        try:
+                child_index_list = children.index(chosen_child_activity[0])
+        except ValueError:
+                pass
+        if symbol == key.RIGHT:
+                try:
+                        chosen_child_activity[0] = children[(child_index_list + 1)]
+                        initial_text = create_initial_text()
+                        game_label.text = initial_text
+                except IndexError:
+                        chosen_child_activity[0] = children[0]
+                        initial_text = create_initial_text()
+                        game_label.text = initial_text
+        elif symbol == key.LEFT:
+                try:
+                        chosen_child_activity[0] = children[child_index_list - 1]
+                        initial_text = create_initial_text()
+                        game_label.text = initial_text
+                except IndexError:
+                        chosen_child_activity[0] = children[len(children)]
+                        initial_text = create_initial_text()
+                        game_label.text = initial_text
+        elif symbol == key._1:
+                chosen_child_activity[1] = activities[0]
+        elif symbol == key._2:
+                chosen_child_activity[1] = activities[1]
+        elif symbol == key._3:
+                chosen_child_activity[1] = activities[2]
+        elif symbol == key._4:
+                chosen_child_activity[1] = activities[3]
+        if chosen_child_activity[1] != None:
+            if symbol == key.A:
+                    chosen_child_activity[1].food = meals[0]
+            elif symbol == key.B:
+                    chosen_child_activity[1].food = meals[1]      
+            elif symbol == key.C:
+                    chosen_child_activity[1].food = meals[2]
+            elif symbol == key.D:
+                    chosen_child_activity[1].food = meals[3]
+            elif symbol == key.E:
+                    chosen_child_activity[1].toy = toys[0]
+            elif symbol == key.F:
+                    chosen_child_activity[1].toy = toys[1]      
+            elif symbol == key.G:
+                    chosen_child_activity[1].toy = toys[2]
+            elif symbol == key.H:
+                    chosen_child_activity[1].toy = toys[3]
+            elif symbol == key.I:
+                    chosen_child_activity[1].other_child = children[0]
+            elif symbol == key.J:
+                    chosen_child_activity[1].other_child = children[1]      
+            elif symbol == key.K:
+                    chosen_child_activity[1].other_child = children[2]
+            elif symbol == key.L:
+                    chosen_child_activity[1].other_child = children[3]   
+
+#game update
+@window.event
+def obnov_stav(dt):
+    for child in children:
+        child.obnov_stav()  # in case that the child is tired, unhappy or hungry it change it sprite to the sad one.
+    if chosen_child_activity[1] != None:
+        chosen_child_activity[1].child = chosen_child_activity[0] # It sets child argument of activity to the choosen child
+    for child in children:     # It sets the opacity of the chosen child to maximum.
+        if child in chosen_child_activity:
+            child.sprite.opacity = 255
+        else:
+            child.sprite.opacity = 190
+
+   
+    
+    if chosen_child_activity[0] != None and chosen_child_activity[1] != None:
+            chosen_child_activity[1].additional_question(dic_children, game_label)
+            if type(chosen_child_activity[1]) == PlayActivity and chosen_child_activity[1].toy != None:
+                    chosen_child_activity[1].execute(game_label)
+                    reset(chosen_child_activity)
+                    pyglet.clock.schedule_once(reset2, 4)
+            elif type(chosen_child_activity[1]) == EatActivity and chosen_child_activity[1].food != None:
+                    chosen_child_activity[1].execute(game_label)
+                    reset(chosen_child_activity)
+                    pyglet.clock.schedule_once(reset2, 4)
+            elif type(chosen_child_activity[1]) == PlayWithFriend and chosen_child_activity[1].other_child != None:
+                    chosen_child_activity[1].execute(game_label)
+                    reset(chosen_child_activity)
+                    pyglet.clock.schedule_once(reset2, 4)
+            elif type(chosen_child_activity[1]) == SleepActivity:
+                    chosen_child_activity[1].execute(game_label)
+                    reset(chosen_child_activity)
+                    pyglet.clock.schedule_once(reset2, 4)
+
+
+
+
+
+print(pyglet.clock.get_fps())
+
+'''print('There are {} children in your class:'.format(len(children)))
+for child in children:
+    child.introduction()
+    
+for child in children:
+    child.update(dt)'''
+'''chosen_child, chosen_activity = chose_child_activity(dic_children,
+                                                     dic_activities)'''
+'''chosen_activity.change_child_parameter(chosen_child)
+chosen_activity.additional_question(dic_children)
+chosen_activity.execute()
+for child in children:
+    child.change_state()'''
+
+pyglet.clock.schedule_interval(obnov_stav, 1/60)
+
+pyglet.app.run()
+print('Hotovo!')
---
+++ miniskolka_pyglet/miniskolka_functions2.py
@@ -0,0 +1,171 @@
+import pyglet
+from pyglet import clock
+
+
+height = 460
+width = 1280
+
+def play_again():
+    """The function asks user, whether he wants to keep playing."""
+    if input(
+             'Press any key to continue (press \'q\' to quit the game): '
+            ) == 'q':
+        return False
+    else:
+        return True
+
+def update_activity(activities, chosen_activity):
+    pass
+
+
+def chose_child_activity(dic_children, dic_activities):
+    """The function asks user to chose a child and an activity, which the
+    child will do. It returns a tuple (chosen_child, chosen_activity).
+    """
+    while True:
+        try:
+            chose_child = input('Who do you want to take care of. Write '
+                                'the corresponding number {}: '
+                                .format(child_election(dic_children)))
+            chose_activity = input('What do you want {} to do. Write '
+                                   'the corresponding number {}: '
+                                   .format(dic_children[chose_child].name,
+                                           activity_election(dic_activities)))
+        except KeyError:
+            print('Try it again! Write the corresponding numbers.')
+        else:
+            try:
+                chosen_child = dic_children[chose_child]
+                chosen_activity = dic_activities[chose_activity]
+            except KeyError:
+                print('Try it again! Write the corresponding numbers.')
+            else:
+                return (chosen_child, chosen_activity)
+
+
+def toy_election(toys):
+    """Generates string which is used in the method additional question
+    of the PlayActivity class. The string looks like 1) dolly 2) barbie...
+    """
+    cislo = 1
+    elect = ''
+    for toy in toys:
+        elect += '{}) {} '.format(cislo, toy)
+        cislo += 1
+    return elect
+
+
+def food_election(meals):
+    """Generates string which is used in the method additional question
+    of the EatActivity class. The string looks like 1) pancake 2) icecream...
+    """
+    cislo = 1
+    elect = ''
+    for meal in meals:
+        elect += '{}) {} '.format(cislo, meal)
+        cislo += 1
+    return elect
+
+
+def create_dic_children(children):
+    """Creates a dictionary of children,
+    like {'1': 'child1, '2': 'child2'...}
+    """
+    number_children = []
+    for number in range(1, len(children)+1):
+        number_children.append(str(number))
+    dic_children = {}
+    for child, number in zip(children, number_children):
+        dic_children[number] = child
+    return dic_children
+
+
+def create_dic_activities(activities):
+    """Creates a dictionary of activities,
+    like {'1': 'eat', '2': 'play'...}
+    """
+    number_activities = []
+    for number in range(1, len(activities)+1):
+        number_activities.append(str(number))
+    dic_activities = {}
+    for activity, number in zip(activities, number_activities):
+        dic_activities[number] = activity
+    return dic_activities
+
+
+def child_election(dic_children):
+    """The function generates string, which is used in chose_child_activity
+    function. It returns name of the children, with corresponding number
+    (1) Maruska, 2) Kacenka...
+    """
+    elect = ''
+    for number in dic_children:
+        elect += '{}) {} '.format(number, dic_children[number].name)
+    return elect
+
+
+def activity_election(dic_activities):
+    """The function generates string, which is used in chose_child_activity
+    function. It returns name of the activities, with corresponding number
+    (1) sleep, 2) play...
+    """
+    elect = ''
+    for number in dic_activities:
+        elect += '{}) {} '.format(number, dic_activities[number].name)
+    return elect
+
+
+def create_batch_labels(children):
+    """Create batch of the labels, which displays states of the children"""
+    i = 1
+    batch_labels = pyglet.graphics.Batch()
+    for child in children:
+        pyglet.text.Label('{}: '.format(child.name), x=50,
+                          y=490 - i*19, bold=True, font_size=14,
+                          font_name="Times New Roman", batch=batch_labels)
+        pyglet.text.Label('mood: {}'.format('*' * child.mood), x=135,
+                          y=490 - i*19, font_size=14,
+                          font_name="Times New Roman", bold=True,
+                          batch=batch_labels)
+        pyglet.text.Label('energy: {}'.format('*' * child.energy), x=260,
+                          y=490 - i*19, font_size=14,
+                          font_name="Times New Roman",
+                          bold=True, batch=batch_labels)
+        pyglet.text.Label('hunger: {}'.format('*' * child.hunger), x=390,
+                          y=490 - i*19, font_size=14,
+                          font_name="Times New Roman",
+                          bold=True, batch=batch_labels)
+        i += 1
+    batch_labels.draw()
+    
+def create_icons_children(children):
+    i = 1
+    icons = []
+    for child in children:
+        small_icon = pyglet.sprite.Sprite(child.img_happy, x=35, y=490 - i*19)
+        small_icon.scale = 0.11
+        icons.append(small_icon)
+        i += 1
+    return icons
+
+def children_set_position(children):
+    """It sets inicial position of the children, which is in a row,
+    moreless in the middle of the classroom.
+    """
+    i = 0
+    for child in children:
+        child.sprite.x = width//2.5 + i*100
+        i += 1
+
+
+def reset(chosen_child_activity):
+    if chosen_child_activity[1] != None:
+        chosen_child_activity[1].food = None
+        chosen_child_activity[1].toy = None
+        chosen_child_activity[1].other_child = None
+    chosen_child_activity[1] = None
+
+
+
+def wait():
+    pass
